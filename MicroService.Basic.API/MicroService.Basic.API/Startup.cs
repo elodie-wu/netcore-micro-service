af81eca45fe0d9ffafd9a1fab4391f6ab1e70f94
wudi <watercolours@qq.com>
2020-10-16 13:30:38 +0800

    feat(basic):add jwt

--- MicroService.Basic.API/MicroService.Basic.API/Startup.cs
+++ MicroService.Basic.API/MicroService.Basic.API/Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using MicroService.Common.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,7 +14,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
 namespace MicroService.Basic.API
 {
     public class Startup
@@ -33,26 +38,65 @@ namespace MicroService.Basic.API
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }); 
                 var filePath = Path.Combine(System.AppContext.BaseDirectory, "MicroService.Basic.API.xml");
                 c.IncludeXmlComments(filePath);
-
+                 
+                c.OperationFilter<AddResponseHeadersFilter>();
+                c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>(); 
                 c.OperationFilter<SecurityRequirementsOperationFilter>();
-
-                #region 开启验证
+                 
+                #region 开启授权
                 c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
                 {
-                    Description = "授权测试",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
+                    Description = "JWT授权Bearer",
+                    Name = "Authorization", 
+                    In = ParameterLocation.Header, 
                     Type = SecuritySchemeType.ApiKey
-                });
+                }); 
                 #endregion
-
             });
+            #endregion
 
-           
+            #region jwt官方认证
+            //出现401说明配置对了
+            //这个是认证
+            var audienceConfig = Configuration.GetSection("Audience"); 
+            var symmetricKeyAsBase64 = "asjdhfjkasdhkflhkashd";
+            var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
+            var signingKey = new SymmetricSecurityKey(keyByteArray);  
+
+            // 令牌验证参数
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true, 
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true, 
+                ValidIssuer = audienceConfig["Issuer"],//发行人
+                ValidateAudience = true, 
+                ValidAudience = audienceConfig["Audience"],//订阅人
+                ValidateLifetime = true, 
+                ClockSkew = TimeSpan.FromSeconds(30),//注意这是缓冲过期时间，总的有效时间等于这个时间加上jwt的过期时间，如果不配置，默认是5分钟
+                RequireExpirationTime = true,
+            }; 
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) 
+             .AddJwtBearer(o =>
+             {
+                 o.TokenValidationParameters = tokenValidationParameters;
+                 o.Events = new JwtBearerEvents
+                 {
+                     OnAuthenticationFailed = context =>
+                     {
+                         // 如果过期，则把<是否过期>添加到，返回头信息中
+                         if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                         {
+                             context.Response.Headers.Add("Token-Expired", "true");
+                         }
+                         return Task.CompletedTask;
+                     }
+                 };
+             });
 
             #endregion
 
